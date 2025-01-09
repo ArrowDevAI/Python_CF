e870c5cde7cb0190ad823f7ff1c2f7879aaf55ea
Trevor Higgins <higginst.th@gmail.com>
2025-01-09 15:06:38 -0500

    Exercise 2.6

--- Django/a2_recipe_app/src/recipe_project/settings.py
+++ Django/a2_recipe_app/src/recipe_project/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'recipe_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL='/login/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
@@ -128,7 +129,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT= BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- Django/a2_recipe_app/src/recipes/views.py
+++ Django/a2_recipe_app/src/recipes/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def Main(request):
     return render(request, 'recipes/home.html')
 
-class RecipeListView(ListView):
+
+class RecipeListView(LoginRequiredMixin,ListView):
     model = Recipe
     template_name = "recipes/list.html"
 
-class RecipeDetailView(DetailView):                       
+class RecipeDetailView(LoginRequiredMixin,DetailView):                       
     model = Recipe                                        
     template_name = 'recipes/detail.html' 
